9ffb0eba4a194d720e1ad83863462c8292b999d4
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-05-29 11:54:08 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,6 +1,10 @@
+import os
 from os import getenv
 
-API_ID = int(getenv("API_ID", ""))
-API_HASH = getenv("API_HASH", "")
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", ""))
+API_ID = int(os.environ.get("API_ID"))
+API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+BOT_USERNAME = os.environ.get("BOT_USERNAME")
+OWNER_ID = int(os.environ.get("OWNER_ID"))
+LOGGER_ID = int(os.environ.get("LOGGER_ID"))
+MONGO_URL = os.environ.get("MONGO_URL")
