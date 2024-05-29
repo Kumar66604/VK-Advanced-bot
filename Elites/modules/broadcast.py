de2069f993632d5cfe24cc043685491e8c04bc43
VinayKumar <150323600+Kumar66604@users.noreply.github.com>
2024-05-29 14:27:10 +0530

    Create broadcast.py

---
+++ Elites/modules/broadcast.py
@@ -0,0 +1,109 @@
+from pymongo import MongoClient
+from config import MONGO_URI
+
+client = MongoClient(MONGO_URI)
+
+users = client['main']['users']
+groups = client['main']['groups']
+
+def already_db(user_id):
+        user = users.find_one({"user_id" : str(user_id)})
+        if not user:
+            return False
+        return True
+
+def already_dbg(chat_id):
+        group = groups.find_one({"chat_id" : str(chat_id)})
+        if not group:
+            return False
+        return True
+
+def add_user(user_id):
+    in_db = already_db(user_id)
+    if in_db:
+        return
+    return users.insert_one({"user_id": str(user_id)}) 
+
+def remove_user(user_id):
+    in_db = already_db(user_id)
+    if not in_db:
+        return 
+    return users.delete_one({"user_id": str(user_id)})
+    
+def add_group(chat_id):
+    in_db = already_dbg(chat_id)
+    if in_db:
+        return
+    return groups.insert_one({"chat_id": str(chat_id)})
+
+def all_users():
+    user = users.find({})
+    usrs = len(list(user))
+    return usrs
+
+def all_groups():
+    group = groups.find({})
+    grps = len(list(group))
+    return grps
+
+@app.on_message(filters.command("broadcast") & filters.user(cfg.SUDO))
+async def bcast(_, m : Message):
+    allusers = users
+    lel = await m.reply_text("`⚡️ Processing...`")
+    success = 0
+    failed = 0
+    deactivated = 0
+    blocked = 0
+    for usrs in allusers.find():
+        try:
+            userid = usrs["user_id"]
+            #print(int(userid))
+            if m.command[0] == "broadcast":
+                await m.reply_to_message.copy(int(userid))
+            success +=1
+        except FloodWait as ex:
+            await asyncio.sleep(ex.value)
+            if m.command[0] == "broadcast":
+                await m.reply_to_message.copy(int(userid))
+        except errors.InputUserDeactivated:
+            deactivated +=1
+            remove_user(userid)
+        except errors.UserIsBlocked:
+            blocked +=1
+        except Exception as e:
+            print(e)
+            failed +=1
+
+    await lel.edit(f"✅Successfull to `{success}` users.\n❌ Faild to `{failed}` users.\n👾 Found `{blocked}` Blocked users \n👻 Found `{deactivated}` Deactivated users.")
+
+
+@app.on_message(filters.command("fcast") & filters.user(cfg.SUDO))
+async def fcast(_, m : Message):
+    allusers = users
+    lel = await m.reply_text("`⚡️ Processing...`")
+    success = 0
+    failed = 0
+    deactivated = 0
+    blocked = 0
+    for usrs in allusers.find():
+        try:
+            userid = usrs["user_id"]
+            #print(int(userid))
+            if m.command[0] == "fcast":
+                await m.reply_to_message.forward(int(userid))
+            success +=1
+        except FloodWait as ex:
+            await asyncio.sleep(ex.value)
+            if m.command[0] == "fcast":
+                await m.reply_to_message.forward(int(userid))
+        except errors.InputUserDeactivated:
+            deactivated +=1
+            remove_user(userid)
+        except errors.UserIsBlocked:
+            blocked +=1
+        except Exception as e:
+            print(e)
+            failed +=1
+
+    await lel.edit(f"✅Successfull to `{success}` users.\n❌ Faild to `{failed}` users.\n👾 Found `{blocked}` Blocked users \n👻 Found `{deactivated}` Deactivated users.")
+          
